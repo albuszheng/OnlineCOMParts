51054e18a40df4536eb6d35e7d0eae0c4cd377db
Keyang Zheng <albuszheng@hotmail.com>
2017-04-16 15:43:12 -0400

    fixing bugs

--- website/resources/views/payment/order_fail.blade.php
+++ website/resources/views/payment/order_fail.blade.php
@@ -9,9 +9,9 @@
         <h2 class="order-fail">Opps, something went wrong...</h2>
         <p class="detailed-info">
             Sorry, we can't finish your order right now, we have not charged you of the total price,
-            you could click <a href="/product_list" here to go back to the product list and re-order again.
+            you could click <a href="/product_list">here</a>  to go back to the product list and re-order again.
             <br />
-            Again, sorry for the inconvinence...
+            Again, sorry for the inconvenience...
         </p>
     </div>
 @endsection
