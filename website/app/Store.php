4daebc0c276f02179e450a23c6d1a912df630c0a
haozhi0209 <haozhi0209@gmail.com>
2017-04-19 20:13:36 -0400

    new

--- website/app/Store.php
+++ website/app/Store.php
@@ -3,8 +3,37 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Store extends Model
 {
-    //
+    protected $table = 'store';
+    protected $primaryKey = 'id';
+
+
+    public $timestamps = false;
+
+    public static function getStoreByRegion($regionID) {
+        $stores = DB::table('store')
+            ->join('regions', 'regions.id', '=', 'store.RegionID')
+            ->whereIn('RegionID', $regionID)
+            ->get();
+        return $stores;
+    }
+
+    public static function getStoreByName($name) {
+        $stores = DB::table('store')
+            ->where('Name', 'like', $name)
+            ->get();
+        return $stores;
+    }
+
+    public static function getStoreByID($id) {
+        $stores = DB::table('store')
+            ->where('id', $id)
+            ->get();
+        return $stores;
+    }
+
+
 }
